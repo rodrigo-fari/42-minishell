fb0137559d530e8104274e1f9b3743093649de8f
Rodrigo-fari <rodrigofal44@gmail.com>
2025-01-08 14:00:14 +0000

    R 08/01/2025 13:59 : Log.txt updated + beginning of the work (Checkpoint)

--- src/ms_tokenizer/tk_print_tokens.c
+++ src/ms_tokenizer/tk_print_tokens.c
@@ -6,7 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 14:51:45 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/01/02 12:43:33 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/01/08 13:44:54 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 void	print_tokens(t_token *token)
 {
 	t_token	*token_tmp;
+	int		i;
 
 	token_tmp = token;
 	printf("------------------------------\n");
+	i = 1;
 	while (token_tmp)
 	{
-		printf("Token[%d] = %s\n", token_tmp->type, token_tmp->value);
+		printf("Token[%d] = %s\n", i, token_tmp->value);
 		token_tmp = token_tmp->next;
+		i++;
 	}
 	printf("------------------------------\n");
 }
