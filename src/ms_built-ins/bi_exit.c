f625d93dcd9daffb2082500537995c7fb61fd1d1
Rodrigo-fari <rodrigofal44@gmail.com>
2025-01-20 13:55:32 +0000

    R 20/01/2025 13:54 : Tester added to the minishell prject. Trying to solve invalid read of size 1 problem

--- src/ms_built-ins/bi_exit.c
+++ src/ms_built-ins/bi_exit.c
@@ -6,7 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 12:22:57 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/01/08 15:08:45 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/01/20 12:59:10 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,21 @@
 void	bi_exit(t_token *tokens, t_env *env, char **commands)
 {
 	if (tokens)
+	{
 		free_token_struct(tokens);
+		tokens = NULL;
+	}
 	if (commands)
+	{
 		free_splits(commands);
+		commands = NULL;
+	}
 	if (env)
+	{
 		free_env_struct(env);
+		env = NULL;
+		env_manager(env);
+	}
 	printf("exit\n");
 	exit(0);
 }
--- src/ms_main/ms_exec.c
+++ src/ms_main/ms_exec.c
@@ -6,7 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 11:19:43 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/01/08 15:13:35 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/01/20 13:51:08 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	ms_exec(char *input, t_env *env)
 {
 	char	**commands;
 	t_token	*tokens;
-	t_token	*tmp;
 
 	commands = tk_splitter(input, 0, 0);
 	if (!ps_parsing(commands, 0))
@@ -27,8 +26,6 @@ void	ms_exec(char *input, t_env *env)
 	remove_quotes(commands);
 	tokens = token_to_struct(commands);
 	free(input);
-	tmp = tokens;
-	print_tokens(tmp);
 	bi_exec(env, tokens, commands);
 	return ;
 }
