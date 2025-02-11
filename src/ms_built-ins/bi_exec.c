c8c3b6e4950acc46970146fe9efb9c55323387cd
Rodrigo-fari <rodrigofal44@gmail.com>
2025-02-11 19:35:50 +0000

    R 11/02/2025 19:35 : Checkpoint

--- src/ms_built-ins/bi_exec.c
+++ src/ms_built-ins/bi_exec.c
@@ -6,7 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 12:18:54 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/01/23 14:07:45 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/02/11 19:30:16 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ void	bi_exec(t_env *env, t_token *tokens, char **commands)
 	env = env_manager(NULL);
 	if (ft_strcmp(tmp->value, "echo") == 0)
 		bi_echo(tmp);
-	// else if (ft_strcmp(tmp->value, "pwd") == 0)
-	// 	bi_pwd();
+	else if (ft_strcmp(tmp->value, "pwd") == 0)
+		bi_pwd();
 	else if (ft_strcmp(tmp->value, "exit") == 0)
 		bi_exit(tmp, env, commands);
 	else if (ft_strcmp(tmp->value, "env") == 0)
 		print_env(env);
-	// else if (ft_strcmp(tmp->value, "cd") == 0)
-	// 	bi_cd(tmp, env);
-	// else if (ft_strcmp(tmp->value, "unset") == 0)
-	// 	bi_unset(tmp, env);
-	// else if (ft_strcmp(tmp->value, "export") == 0)
-	// 	bi_export(env, tmp);
+	else if (ft_strcmp(tmp->value, "cd") == 0)
+		bi_cd(commands, env);
+	else if (ft_strcmp(tmp->value, "unset") == 0)
+		bi_unset(commands, env);
+	else if (ft_strcmp(tmp->value, "export") == 0)
+		bi_export(commands, tmp);
 	else
 		exec_exe(tmp, env, commands);
 }
