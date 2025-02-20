f4d5b88f44884780236d212a30a61eab1fa5215d
Rodrigo-fari <rodrigofal44@gmail.com>
2025-02-20 14:10:00 +0000

    R 20/02/2025 14:09 : Small bug found and fixed just pushing things up right this time :)

--- src/ms_sighand/sg_sig_treatment.c
+++ src/ms_sighand/sg_sig_treatment.c
@@ -6,13 +6,13 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 12:12:38 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/02/20 14:06:16 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/02/20 14:09:08 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	sig_ctrl_c(int sig)
+// void	sig_ctrl_c(int sig)
 // {
 // 	(void)sig;
 // 	write(1, "\n", 1);
