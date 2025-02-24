53fc8888024f21d40dcc5009225f06a89ff8c564
Rodrigo-fari <rodrigofal44@gmail.com>
2025-02-24 17:00:26 +0000

    R 24/02/2025 17:00 : Checkpoint to fix whole project norminette

--- src/ms_parsing/ps_expand_exit_status.c
+++ src/ms_parsing/ps_expand_exit_status.c
@@ -6,72 +6,70 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 13:48:52 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/02/24 15:41:42 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/02/24 16:59:42 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-#include "minishell.h"
-
 void	replace_exit_status_in_command(char **command)
 {
-    char	*pos;
+	char	*pos;
 
-    pos = ft_strnstr(*command, "$?", ft_strlen(*command));
-    while (pos)
-    {
-        replace_exit_status_at_pos(command, pos);
-        pos = ft_strnstr(*command, "$?", ft_strlen(*command));
-    }
+	pos = ft_strnstr(*command, "$?", ft_strlen(*command));
+	while (pos)
+	{
+		replace_exit_status_at_pos(command, pos);
+		pos = ft_strnstr(*command, "$?", ft_strlen(*command));
+	}
 }
 
 void	replace_exit_status_at_pos(char **command, char *pos)
 {
-    char	*expanded_str;
-    char	*remaining_str;
-    char	*itoa_str;
-    char	*temp;
-    char	*substr;
+	char	*expanded_str;
+	char	*remaining_str;
+	char	*itoa_str;
+	char	*temp;
+	char	*substr;
 
-    itoa_str = ft_itoa(g_exit_status);
-    substr = ft_substr(*command, 0, pos - *command);
-    expanded_str = ft_strjoin(substr, itoa_str);
-    remaining_str = ft_strdup(pos + 2);
-    temp = *command;
-    *command = ft_strjoin(expanded_str, remaining_str);
-    free(temp);
-    free(expanded_str);
-    free(remaining_str);
-    free(itoa_str);
-    free(substr);
+	itoa_str = ft_itoa(g_exit_status);
+	substr = ft_substr(*command, 0, pos - *command);
+	expanded_str = ft_strjoin(substr, itoa_str);
+	remaining_str = ft_strdup(pos + 2);
+	temp = *command;
+	*command = ft_strjoin(expanded_str, remaining_str);
+	free(temp);
+	free(expanded_str);
+	free(remaining_str);
+	free(itoa_str);
+	free(substr);
 }
 
 void	replace_exit_status(char **commands, int i)
 {
-    replace_exit_status_in_command(&commands[i]);
+	replace_exit_status_in_command(&commands[i]);
 }
 
 void	expand_exit(char **commands)
 {
-    int		i;
-    int		j;
-    int		quotes;
+	int		i;
+	int		j;
+	int		quotes;
 
-    i = 0;
-    while (commands[i])
-    {
-        quotes = 0;
-        j = 0;
-        while (commands[i][j])
-        {
-            if (commands[i][j] == '"')
-                quotes = !quotes;
-            if (!quotes)
-                replace_exit_status(commands, i);  // Substitui "$?" fora das aspas
-            j++;
-        }
-        i++;
-    }
+	i = 0;
+	while (commands[i])
+	{
+		quotes = 0;
+		j = 0;
+		while (commands[i][j])
+		{
+			if (commands[i][j] == '"')
+				quotes = !quotes;
+			if (!quotes)
+				replace_exit_status(commands, i);  // Substitui "$?" fora das aspas
+			j++;
+		}
+		i++;
+	}
 }
 
