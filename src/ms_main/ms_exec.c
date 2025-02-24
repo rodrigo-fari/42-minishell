4dc89273e22c940a21209870a22a4e11d9c1231e
Rodrigo-fari <rodrigofal44@gmail.com>
2025-02-24 16:59:03 +0000

    R 124/02/2025 16:58 : Variable expansion working with quotes. Still need to fix norminette, function with more than 25 lines

--- src/ms_main/ms_exec.c
+++ src/ms_main/ms_exec.c
@@ -6,7 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 11:19:43 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/02/24 14:14:14 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/02/24 16:39:49 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ void	ms_exec(char *input, t_env *env)
 		ms_free(NULL, input, commands, NULL);
 		return ;
 	}
+	replace_env_variables(commands);
 	expand_exit(commands);
 	remove_quotes(commands);
-	replace_env_variables(commands);
 	tokens = token_to_struct(commands);
 	// print_tokens(tokens);
 	bi_exec(env, tokens, commands);
--- src/ms_parsing/ps_var_expand_2.c
+++ src/ms_parsing/ps_var_expand_2.c
@@ -6,46 +6,69 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 15:27:20 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/02/20 16:34:55 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/02/24 16:41:58 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*expand_variables(char *str)
+char *append_char_to_str(char *str, char c)
 {
-	char	*result;
-	char	*start;
-	char	*end;
-	char	*var_value;
-	size_t	old_size;
+	size_t len = strlen(str);
+	char *new_str = realloc(str, len + 2);
+	if (!new_str)
+		return NULL;
+	new_str[len] = c;
+	new_str[len + 1] = '\0';
+	return new_str;
+}
+
+char *expand_variables(char *str)
+{
+	char *result;
+	char *start;
+	char *end;
+	char *var_value;
+	size_t old_size;
+	bool in_single_quotes;
 
 	result = malloc(1);
 	if (!result)
-		return (NULL);
+		return NULL;
 	result[0] = '\0';
 	start = str;
-	while ((end = ft_strchr(start, '$')))
+	in_single_quotes = false;
+	while (*start)
 	{
-		old_size = ft_strlen(result);
-		result = process_segment(result, &start, end);
-		var_value = replace_variable(start, &start);
-		if (var_value)
+		if (*start == '\'')
+			in_single_quotes = !in_single_quotes;
+		if (!in_single_quotes && (end = ft_strchr(start, '$')))
 		{
-			result = ft_realloc(result, old_size + ft_strlen(var_value) + 1);
-			if (!result)
-				return (NULL);
-			ft_strcat(result, var_value);
-			free(var_value);
+			old_size = ft_strlen(result);
+			result = process_segment(result, &start, end);
+			var_value = replace_variable(start, &start);
+			if (var_value)
+			{
+				result = ft_realloc(result, old_size + ft_strlen(var_value) + 1);
+				if (!result)
+					return NULL;
+				ft_strcat(result, var_value);
+				free(var_value);
+			}
+		}
+		else
+		{
+			result = append_char_to_str(result, *start);
+			start++;
 		}
 	}
-	return (append_str(result, start));
+	return result;
 }
 
-void	replace_env_variables(char **commands)
+void replace_env_variables(char **commands)
 {
-	int		i;
-	char	*result;
+	int i;
+	char *result;
 
 	i = 0;
 	while (commands[i])
